2a655af7ed80afa9b5674b8f45793b8c28e794d7
Heder-Silva-Oliveira <hedersilva2@gmail.com>
2021-06-25 17:51:46 -0300

    002:

--- ola_django/urls.py
+++ ola_django/urls.py
@@ -17,6 +17,7 @@ from django.contrib import admin
 from django.urls import path
 from core import views
 
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('hello/<nome>/', views.hello)
