6173ad013ee791ac35633122334b936eed997868
Heder-Silva-Oliveira <hedersilva2@gmail.com>
2021-08-07 17:44:56 -0300

    006:

--- core/views.py
+++ core/views.py
@@ -4,4 +4,4 @@ from django.shortcuts import render, HttpResponse
 # Create your views here.
 
 def hello(requet, nome):
-    return HttpResponse('<h1>Hello Word {}</h1>'.format(nome))
+    return HttpResponse('<h1>Hello Word | Olá mundo {}</h1>'.format(nome))
