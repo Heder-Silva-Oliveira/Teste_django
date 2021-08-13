e19863d40f0b806eaabef5018bdd9adb830a6eb0
Heder-Silva-Oliveira <hedersilva2@gmail.com>
2021-08-13 11:43:33 -0300

    013

--- core/views.py
+++ core/views.py
@@ -1,12 +1,13 @@
-from django.shortcuts import render, HttpResponse
+from django.shortcuts import render, HttpResponse, redirect
 from core.models import Evento
 
 
 # Create your views here.
 
+def index(request):
+    return redirect('/ola_django')
 
 def lita_eventos(request):
-    usuario = request.user
-    evento = Evento.objects.filter(usuario=usuario)
+    evento = Evento.objects.all()
     dados = {'eventos', evento}
     return render(request, 'agenda.html', dados)
