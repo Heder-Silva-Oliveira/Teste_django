b212ffa14afcee0cfa565a44f636f262a700a12e
Heder-Silva-Oliveira <hedersilva2@gmail.com>
2021-09-27 19:17:17 -0300

    020

--- core/models.py
+++ core/models.py
@@ -7,9 +7,10 @@ from datetime import datetime, timedelta
 class Evento(models.Model):
     titulo = models.CharField(max_length=100)
     descricao = models.TextField(blank=True, null=True)
-    data_evento = models.DateTimeField(verbose_name="Data do Evento")
+    data_evento = models.DateTimeField(verbose_name='Data do Evento')
     data_criacao = models.DateTimeField(auto_now=True)
     usuario = models.ForeignKey(User, on_delete=models.CASCADE)
+
 
     class Meta:
         db_table = 'evento'
@@ -19,3 +20,12 @@ class Evento(models.Model):
 
     def get_data_evento(self):
         return self.data_evento.strftime('%d/%m/%Y %H:%M Hrs')
+
+    def get_data_input_evento(self):
+        return self.data_evento.strftime('%Y-%m-%dT%H:%M')
+
+    def get_evento_atrasado(self):
+        return self.data_evento < datetime.now()
+
+    def get_evento_que_falta_menos_de_1h(self):
+        return datetime.now() > self.data_evento - timedelta(hours=1) and datetime.now() < self.data_evento
